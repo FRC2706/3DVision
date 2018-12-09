661a566163b68acafadce355014a422e703f10d3
thatmadhacker <finman292004@gmail.com>
2018-12-09 09:58:20 -0500

    Tweaked some stuff but video and depth still dont work

--- src/ca/team2706/vision3d/Main.java
+++ src/ca/team2706/vision3d/Main.java
@@ -1,5 +1,7 @@
 package ca.team2706.vision3d;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -9,88 +11,96 @@ import java.nio.ByteBuffer;
 import javax.imageio.ImageIO;
 
 import org.openkinect.freenect.Context;
+import org.openkinect.freenect.DepthFormat;
 import org.openkinect.freenect.DepthHandler;
 import org.openkinect.freenect.Device;
 import org.openkinect.freenect.FrameMode;
 import org.openkinect.freenect.Freenect;
 import org.openkinect.freenect.LogLevel;
+import org.openkinect.freenect.VideoFormat;
 import org.openkinect.freenect.VideoHandler;
 import org.openkinect.freenect.util.Jdk14LogHandler;
 
 public class Main {
 
 	private static Context ctx;
-    private static Device dev;
-    private static DisplayGui depth = null;
-    private static DisplayGui rgb = null;
-	
+	private static Device dev;
+
 	public static void main(String[] args) throws InterruptedException {
 		initKinect();
-		
-		//startDepth(); these dont work atm
-		//startVideo();
-		
+
+		startDepth();
+		startVideo();
+
 		shutdownKinect();
 	}
-	
+
 	public static void startDepth() {
+		dev.setDepthFormat(DepthFormat.D10BIT);
 		dev.startDepth(new DepthHandler() {
 
-            @Override
-            public void onFrameReceived(FrameMode mode, ByteBuffer frame, int timestamp) {
-            	try {
-            		BufferedImage image = byteArrayToImage(frame.array());
-            		if(depth == null) {
-            			depth = new DisplayGui(image,"depth",true);
-            		}
-            		depth.updateImage(image);
-            	}catch(Exception e) {
-            		e.printStackTrace();
-            	}
-            }
-        });
+			@Override
+			public void onFrameReceived(FrameMode mode, ByteBuffer frame, int timestamp) {
+				try {
+
+					while (true) {
+
+						try {
+							int i = frame.getInt();
+							
+							System.out.println(i);
+							
+						} catch (Exception e) {
+							e.printStackTrace();
+							break;
+						}
+
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
+
 	public static void startVideo() {
+		dev.setVideoFormat(VideoFormat.RGB);
 		dev.startVideo(new VideoHandler() {
-            @Override
-            public void onFrameReceived(FrameMode mode, ByteBuffer frame, int timestamp) {
-            	try {
-            		BufferedImage image = byteArrayToImage(frame.array());
-            		if(rgb == null) {
-            			rgb = new DisplayGui(image,"rgb",true);
-            		}
-            		rgb.updateImage(image);
-            	}catch(Exception e) {
-            		e.printStackTrace();
-            	}
-            }
-        });
+			@Override
+			public void onFrameReceived(FrameMode mode, ByteBuffer frame, int timestamp) {
+				try {
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
+
 	public static BufferedImage byteArrayToImage(byte[] array) throws IOException {
 		InputStream in = new ByteArrayInputStream(array);
 		BufferedImage bImageFromConvert = ImageIO.read(in);
 		return bImageFromConvert;
 	}
-	
+
 	public static void initKinect() {
-        ctx = Freenect.createContext();
-        ctx.setLogHandler(new Jdk14LogHandler());
-        ctx.setLogLevel(LogLevel.SPEW);
-        if (ctx.numDevices() > 0) {
-            dev = ctx.openDevice(0);
-        } else {
-            System.err.println("WARNING: No kinects detected, hardware tests will be implicitly passed.");
-        }
-    }
-	
+		ctx = Freenect.createContext();
+		ctx.setLogHandler(new Jdk14LogHandler());
+		ctx.setLogLevel(LogLevel.SPEW);
+		if (ctx.numDevices() > 0) {
+			dev = ctx.openDevice(0);
+		} else {
+			System.err.println("WARNING: No kinects detected.");
+		}
+	}
+
 	public static void shutdownKinect() {
-        if (ctx != null)
-            if (dev != null) {
-                dev.close();
-            }
-        ctx.shutdown();
-    }
+		if (ctx != null)
+			if (dev != null) {
+				dev.close();
+			}
+		ctx.shutdown();
+	}
 
 }
